2b0e1357f08cd0c1ae64d06bb674e9986271d0d5
Aaron Li-Mai <Aaron.Li-Mai@calgarycounselling.com>
2018-08-09 08:57:56 -0600

    Updated TestController class

--- WebApplication1/Controllers/TestController.cs
+++ WebApplication1/Controllers/TestController.cs
@@ -7,7 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Newtonsoft.Json;
-using GroupQuestionnaireApp;
+using WebApplication1.Models;
 using System.Web.Script.Serialization;
 
 namespace WebApplication1.Controllers
@@ -15,6 +15,17 @@ namespace WebApplication1.Controllers
     [OutputCacheAttribute(VaryByParam = "*", Duration = 0, NoStore = true)]
     public class TestController : BaseController
     {
-        
+        private RequestContext _homeContext;
+
+        public RequestContext HomeContext
+        {
+            set { _homeContext = value; }
+        }
+
+        [HttpPost]
+        public ActionResult SelectGroupPackage (SelectGroupPackageModel viewModel)
+        {
+            
+        }
     }
 }
