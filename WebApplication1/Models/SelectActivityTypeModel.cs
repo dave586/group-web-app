71a72b33107aade79ba949979a04355966006f92
Aaron Li-Mai <Aaron.Li-Mai@calgarycounselling.com>
2018-08-09 10:39:26 -0600

    Added new class models

---
+++ WebApplication1/Models/SelectActivityTypeModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using GroupQuestionnaireApp.EFModel;
+
+namespace WebApplication1.Models
+{
+    public class SelectActivityTypeModel
+    {
+        public List<GroupActivityType> Activity { get; set; }
+        public SelectActivityTypeModel()
+        {
+            Activity = new List<GroupActivityType>();
+        }
+
+        public static SelectActivityTypeModel GenerateSelectTestModel()
+        {
+            SelectActivityTypeModel satm = new SelectActivityTypeModel();
+
+            using (OQDevSNAPEntities dbContext = new OQDevSNAPEntities())
+            {
+                var oqTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "OQ");
+
+            }
+            return satm;
+        }
+    }
+}
---
+++ WebApplication1/Models/SelectGroupProgram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using GroupQuestionnaireApp.EFModel;
+
+namespace WebApplication1.Models
+{
+    public class SelectGroupProgramModel
+    {
+        [Required]
+        public int ClientID { get; set; }
+        [Required]
+        public int IntakeFileID { get; set; }
+        public List<GroupProgram> Program { get; set; }
+
+        public SelectGroupProgramModel()
+        {
+            Program = new List<GroupProgram>();
+        }
+
+        public static SelectGroupProgramModel GenerateSelectGroupProgramModel(int clientID, int intakeFileID)
+        {
+            SelectGroupProgramModel gp = new SelectGroupProgramModel { ClientID = clientID, IntakeFileID = intakeFileID };
+
+            using (OQDevSNAPEntities dbContext = new OQDevSNAPEntities())
+            {
+                var was  = dbContext.GroupPrograms.FirstOrDefault(p => p.ProgramName == "WAS");
+                var oed  = dbContext.GroupPrograms.FirstOrDefault(p => p.ProgramName == "OED");
+                var be   = dbContext.GroupPrograms.FirstOrDefault(p => p.ProgramName == "BE");
+                var bld  = dbContext.GroupPrograms.FirstOrDefault(p => p.ProgramName == "BLD");
+                var yna  = dbContext.GroupPrograms.FirstOrDefault(p => p.ProgramName == "YNA");
+                var tftb = dbContext.GroupPrograms.FirstOrDefault(p => p.ProgramName == "TFTB");
+                var rcm  = dbContext.GroupPrograms.FirstOrDefault(p => p.ProgramName == "RCM");
+                var rcw  = dbContext.GroupPrograms.FirstOrDefault(p => p.ProgramName == "RCW");
+
+                var programs = new List<GroupProgram>();
+            }
+            return gp;
+        }
+    }
+}
