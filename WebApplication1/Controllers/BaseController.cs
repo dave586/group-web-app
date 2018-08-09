6446f2d6f7768578eb939969de7dca4246905c43
Aaron Li-Mai <Aaron.Li-Mai@calgarycounselling.com>
2018-08-09 08:45:05 -0600

    Added BaseController class

---
+++ WebApplication1/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication1.Controllers
+{
+    public class BaseController : Controller
+    {
+        
+    }
+}
