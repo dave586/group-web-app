b785b3cd9cd01aeb975e848aac100d0f988fb884
Aaron Li-Mai <Aaron.Li-Mai@calgarycounselling.com>
2018-08-09 08:45:35 -0600

    Renamed and modified TestController class

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Newtonsoft.Json;
+using GroupQuestionnaireApp;
+using System.Web.Script.Serialization;
+
+namespace WebApplication1.Controllers
+{
+    [OutputCacheAttribute(VaryByParam = "*", Duration = 0, NoStore = true)]
+    public class TestController : BaseController
+    {
+        
+    }
+}
