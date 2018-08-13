3bd133fdfce860c5272a61062f9bd08a5e34d393
Aaron Li-Mai <Aaron.Li-Mai@calgarycounselling.com>
2018-08-13 08:37:53 -0600

    Added an extension class for GroupProgram

---
+++ ClassLibrary1/EFModel/Extensions/GroupProgram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupQuestionnaireApp.EFModel
+{
+    public partial class GroupProgram
+    {
+        public int SelectorID { get; set; }
+
+        public virtual GroupProgram Program { get; set; }
+    }
+}
