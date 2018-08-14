96d8037f90d6c47ff6aed7903024f2423222d889
Aaron Li-Mai <Aaron.Li-Mai@calgarycounselling.com>
2018-08-14 12:52:45 -0600

    Added missing javascript files

---
+++ WebApplication1/Scripts/CompleteTest.js
@@ -0,0 +1,38 @@
+﻿
+$(document).ready(function() {
+    $("#submitTest").click(function () {
+            
+        if(model.testAnswers()) {
+            $("#data").val(JSON.stringify(model.viewModel));
+            $("form").submit();
+        }
+    });
+
+    var data = @Html.Raw(Json.Encode(Model));
+
+    var model =
+    {
+        viewModel: data,
+        testAnswers:function () {
+            var allAnswered = true;
+            for(var q=0; q < this.viewModel.Questions.length; q++) {
+                if(ul.utils.isNullOrEmpty(this.viewModel.Questions[q].Answer)) {
+                    allAnswered = false;
+                    break;
+                }
+            }
+                
+            if(!allAnswered) {
+                if(confirm('Some questions have not been answered. Are you sure you want to continue?')) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        }
+    };
+
+    ko.applyBindings(model);
+
+});
