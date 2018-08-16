df923b5345b8ab9cbcb326cffe5b31a76598626e
Aaron Li-Mai <Aaron.Li-Mai@calgarycounselling.com>
2018-08-16 09:09:14 -0600

    Added Signal folder that holds a Package object and a Question object

---
+++ GroupQuestionnaireApp/Signals/Package.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GroupQuestionnaireApp.Signals
+{
+
+    public class Package
+    {
+        public string ProgramName { get; set; }
+        public string ActivityName { get; set; }
+        public string PackageName { get; set; }
+        public List<Activity> Activities { get; set; }
+
+        public Package()
+        {
+            Activities = new List<Activity>();
+        }
+    }
+    public class Activity
+    {
+        public string ActivityName { get; set; }
+        //public string Value { get; set; }
+    }
+}
---
+++ GroupQuestionnaireApp/Signals/Question.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GroupQuestionnaireApp.Signals
+{
+
+    public class Question
+    {
+        public string QuestionText { get; set; }
+        public string QuestionID { get; set; }
+        public string QuestionNumber { get; set; }
+        public List<Option> Options { get; set; }
+        public string Answer { get; set; }
+        public string QuestionType { get; set; }
+
+        public Question()
+        {
+            Options = new List<Option>();
+        }
+    }
+
+
+    //public class SingleChoiceQuestion : Question
+    //{
+    //    //public bool IsAscending { get; set; }
+    //    //public int MinValue { get; set; }
+    //    //public int MaxValue { get; set; }
+
+
+
+    //    public override string QuestionType
+    //    {
+    //        get { return "SingleChoiceQuestion"; }
+    //    }
+
+    //    public SingleChoiceQuestion()
+    //    {
+    //        Options = new List<Option>();
+    //    }
+
+    //}
+
+    public class Answer
+    {
+        public string QuestionID { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class Option
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+    }
+}
