f20b023b21344f96c2f98d8f846fd440844eaa34
Aaron Li-Mai <Aaron.Li-Mai@calgarycounselling.com>
2018-08-16 09:05:09 -0600

    Updated BaseController

--- WebApplication1/Controllers/BaseController.cs
+++ WebApplication1/Controllers/BaseController.cs
@@ -11,6 +11,33 @@ namespace WebApplication1.Controllers
 {
     public class BaseController : Controller
     {
-        
+        protected override void Initialize(RequestContext requestContext)
+        {
+            string culture = requestContext.HttpContext.Session != null && requestContext.HttpContext.Session["culture"] != null ? requestContext.HttpContext.Session["culture"].ToString() : "en-CA";
+            if (!String.IsNullOrEmpty(culture))
+            {
+                SetupCulture(culture, requestContext);
+            }
+            base.Initialize(requestContext);
+        }
+
+        public void SetupCulture(string culture, RequestContext requestContext)
+        {
+            culture = culture ?? "en-CA";
+            ViewBag.Culture = culture;
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(culture);
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture(culture);
+
+            DateTimeFormatInfo englishDateTimeFormat = new CultureInfo("en-CA").DateTimeFormat; //keep datetime format same independent of culture
+            Thread.CurrentThread.CurrentCulture.DateTimeFormat = englishDateTimeFormat;
+
+            if (requestContext != null && requestContext.HttpContext != null)
+                requestContext.HttpContext.Session["culture"] = culture;
+        }
+
+        public string GetCulture(RequestContext requestContext)
+        {
+            return (requestContext != null && requestContext.HttpContext != null && requestContext.HttpContext.Session != null) ? requestContext.HttpContext.Session["culture"].ToString() : "en-CA";
+        }
     }
 }
