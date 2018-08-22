95e22444320d0e53a261cb0f3f00e6a0efa6b514
Aaron Li-Mai <Aaron.Li-Mai@calgarycounselling.com>
2018-08-22 15:44:14 -0600

    Added new variables

--- GroupQuestionnaireApp/Signals/Package.cs
+++ GroupQuestionnaireApp/Signals/Package.cs
@@ -8,18 +8,21 @@ namespace GroupQuestionnaireApp.Signals
     public class Package
     {
         public string ProgramName { get; set; }
-        public string ActivityName { get; set; }
+		public string ActivityID { get; set; }
+		public string ActivityName { get; set; }
         public string PackageName { get; set; }
-        public List<Activity> Activities { get; set; }
+		public string ActivityDisplayName { get; set; }
+        public List<Question> Activities { get; set; }
 
         public Package()
         {
-            Activities = new List<Activity>();
+            Activities = new List<Question>();
         }
     }
     public class Activity
     {
+		public string ActivityID { get; set; }
         public string ActivityName { get; set; }
-        //public string Value { get; set; }
-    }
+        public string ActivityDisplayName { get; set; }
+	}
 }
