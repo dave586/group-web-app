1f9b5df24fabf813e11652fe40c581e0deb014c8
Aaron Li-Mai <Aaron.Li-Mai@calgarycounselling.com>
2018-08-23 08:54:53 -0600

    Added activities

--- WebApplication1/Models/SelectActivityTypeModel.cs
+++ WebApplication1/Models/SelectActivityTypeModel.cs
@@ -11,6 +11,7 @@ namespace WebApplication1.Models
     public class SelectActivityTypeModel
     {
         public List<GroupActivityType> Activity { get; set; }
+
         public SelectActivityTypeModel()
         {
             Activity = new List<GroupActivityType>();
@@ -32,40 +33,40 @@ namespace WebApplication1.Models
                 var sdtTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "SDT");
                 var rseTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "RSE");
                 var pasTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PAS");
-                var tsclTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "TSCL-40");
-                var preRSFVP = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "Pre RS-FVP");
-                var famIII = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "FAM-III");
-                var pasph = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PASPH");
-                var pasnp = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PASNP");
-                var npaps = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "NPAPS");
-                var paps = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PAPS");
-                var ses = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "SES");
-                var ise = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "ISE");
-                var edi3 = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "EDI-3");
-                var csq8 = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "CSQ-8");
-                var pfRCM = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PF-RCM");
-                var dair = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "DAI-R");
-                var cdi2 = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "CDI-2");
-                var sipa = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "SIPA");
-                var psi = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PSI");
-                var marFeed = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "Marriage Feedback");
-                var rcForm = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "RC");
-                var preDemoFVP = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "Pre Demo-FVP");
-                var pes = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PES");
-                var sea = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "SEA Exit");
-                var preWorkDemo = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "Pre Workshop Demo");
-                var demoOED = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "Demo-OED");
-                var pfOED = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PF-OED");
-                var demoBEBLDWAS = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "Demo-BE/BLD/WAS");
-                var pfBE = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PF-BE");
-                var postDemoFVP = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "Post Demo-FVP");
-                var pfTFTB = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PF-TFTB");
-                var postRSFVP = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "Post RS-FVP");
-                var pfYNA = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PF-YNA");
-                var pfBLD = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PF-BLD");
-                var pfWAS = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PF-WAS");
-                var pfRCW = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PF-RCW");
-                var codExit = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "COD Exit");
+                var tscl40Test = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "TSCL-40");
+                var preRSFVPTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "Pre RS-FVP");
+                var famIIITest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "FAM-III");
+                var pasphTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PASPH");
+                var pasnpTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PASNP");
+                var npapsTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "NPAPS");
+                var papsTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PAPS");
+                var sesTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "SES");
+                var iseTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "ISE");
+                var edi3Test = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "EDI-3");
+                var csq8Test = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "CSQ-8");
+                var pfRCMTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PF-RCM");
+                var dairTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "DAI-R");
+                var cdi2Test = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "CDI-2");
+                var sipaTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "SIPA");
+                var psiTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PSI");
+                var marFeedTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "Marriage Feedback");
+                var rcFormTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "RC");
+                var preDemoFVPTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "Pre Demo-FVP");
+                var pesTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PES");
+                var seaTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "SEA Exit");
+                var preWorkDemoTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "Pre Workshop Demo");
+                var demoOEDTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "Demo-OED");
+                var pfOEDTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PF-OED");
+                var demoBEBLDWASTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "Demo-BE/BLD/WAS");
+                var pfBETest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PF-BE");
+                var postDemoFVPTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "Post Demo-FVP");
+                var pfTFTBTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PF-TFTB");
+                var postRSFVPTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "Post RS-FVP");
+                var pfYNATest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PF-YNA");
+                var pfBLDTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PF-BLD");
+                var pfWASTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PF-WAS");
+                var pfRCWTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "PF-RCW");
+                var codExitTest = dbContext.GroupActivityTypes.FirstOrDefault(t => t.ActivityName == "COD Exit");
 
                 var activity = new List<GroupActivityType>();
 
@@ -75,7 +76,116 @@ namespace WebApplication1.Models
                     ActivityDisplayName = oqTest.ActivityDisplayName,
                     ActivityName = oqTest.ActivityName
                 };
+
                 activity.Add(oq);
+
+				GroupActivityType urica = new GroupActivityType()
+				{
+					ActivityTypeID = uricaTest.ActivityTypeID,
+					ActivityDisplayName = uricaTest.ActivityDisplayName,
+					ActivityName = uricaTest.ActivityName
+				};
+
+				activity.Add(urica);
+
+				GroupActivityType pyoq = new GroupActivityType()
+				{
+					ActivityTypeID = pyoqTest.ActivityTypeID,
+					ActivityDisplayName = pyoqTest.ActivityDisplayName,
+					ActivityName = pyoqTest.ActivityName
+				};
+
+				activity.Add(pyoq);
+
+				GroupActivityType yoq = new GroupActivityType()
+				{
+					ActivityTypeID = yoqTest.ActivityTypeID,
+					ActivityDisplayName = yoqTest.ActivityDisplayName,
+					ActivityName = yoqTest.ActivityName
+				};
+
+				activity.Add(yoq);
+
+				GroupActivityType gcs = new GroupActivityType()
+				{
+					ActivityTypeID = gcsTest.ActivityTypeID,
+					ActivityDisplayName = gcsTest.ActivityDisplayName,
+					ActivityName = gcsTest.ActivityName
+				};
+
+				activity.Add(gcs);
+
+				GroupActivityType ics = new GroupActivityType()
+				{
+					ActivityTypeID = icsTest.ActivityTypeID,
+					ActivityDisplayName = icsTest.ActivityDisplayName,
+					ActivityName = icsTest.ActivityName
+				};
+
+				activity.Add(ics);
+
+				GroupActivityType sdt = new GroupActivityType()
+				{
+					ActivityTypeID = sdtTest.ActivityTypeID,
+					ActivityDisplayName = sdtTest.ActivityDisplayName,
+					ActivityName = sdtTest.ActivityName
+				};
+
+				activity.Add(sdt);
+
+				GroupActivityType rse = new GroupActivityType()
+				{
+					ActivityTypeID = rseTest.ActivityTypeID,
+					ActivityDisplayName = rseTest.ActivityDisplayName,
+					ActivityName = rseTest.ActivityName
+				};
+
+				activity.Add(rse);
+
+				GroupActivityType pas = new GroupActivityType()
+				{
+					ActivityTypeID = pasTest.ActivityTypeID,
+					ActivityDisplayName = pasTest.ActivityDisplayName,
+					ActivityName = pasTest.ActivityName
+				};
+
+				activity.Add(pas);
+
+				GroupActivityType tscl40 = new GroupActivityType()
+				{
+					ActivityTypeID = tscl40Test.ActivityTypeID,
+					ActivityDisplayName = tscl40Test.ActivityDisplayName,
+					ActivityName = tscl40Test.ActivityName
+				};
+
+				activity.Add(tscl40);
+
+				GroupActivityType preRSFVP = new GroupActivityType()
+				{
+					ActivityTypeID = preRSFVPTest.ActivityTypeID,
+					ActivityDisplayName = preRSFVPTest.ActivityDisplayName,
+					ActivityName = preRSFVPTest.ActivityName
+				};
+
+				activity.Add(preRSFVP);
+
+				GroupActivityType famIII = new GroupActivityType()
+				{
+					ActivityTypeID = famIIITest.ActivityTypeID,
+					ActivityDisplayName = famIIITest.ActivityDisplayName,
+					ActivityName = famIIITest.ActivityName
+				};
+
+				activity.Add(famIII);
+
+				GroupActivityType pasph = new GroupActivityType()
+				{
+					ActivityTypeID = pasphTest.ActivityTypeID,
+					ActivityDisplayName = pasphTest.ActivityDisplayName,
+					ActivityName = pasphTest.ActivityName
+				};
+
+				activity.Add(pasph);
 
                 satm.Activity = activity;
             }
